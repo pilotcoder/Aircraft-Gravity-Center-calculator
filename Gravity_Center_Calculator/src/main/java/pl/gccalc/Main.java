8f4cbc448952090be5ab1a8eb52fc21f2c6e7481
Marcin <awings@o2.pl>
2023-10-04 19:46:45 +0200

    Main updated

--- Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
+++ Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
@@ -1,9 +1,11 @@
 package pl.gccalc;
 
 public class Main {
+    static Aircraft aircraft = new Aircraft();
     public static void main(String[] args) {
         System.out.println("Hello in calculator please enter correct values:");
 
 
     }
+
 }
