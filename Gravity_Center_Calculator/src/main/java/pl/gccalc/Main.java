a87d0147a63c739bf3fc8b6d36dd97ca91ae3dee
Marcin <awings@o2.pl>
2023-10-05 18:48:00 +0200

    Main updated (first commands)

--- Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
+++ Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
@@ -1,9 +1,22 @@
 package pl.gccalc;
 
+import java.util.Scanner;
+
 public class Main {
     static Aircraft aircraft = new Aircraft();
     public static void main(String[] args) {
         System.out.println("Hello in calculator please enter correct values:");
+        System.out.println("enter l lenght:");
+
+        Scanner scanner = new Scanner(System.in);
+        aircraft.setLLenght(scanner.nextDouble());
+
+        System.out.println("enter a lenght");
+        aircraft.setALenght(scanner.nextDouble());
+
+        System.out.println("enter front weight");
+
+
 
 
     }
