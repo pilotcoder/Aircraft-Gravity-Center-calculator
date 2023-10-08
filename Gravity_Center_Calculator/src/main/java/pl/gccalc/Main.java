daa0911ea0e459a5eaf802ddcb8f76f639eb1dc6
Marcin <awings@o2.pl>
2023-10-08 22:06:49 +0200

    update

--- Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
+++ Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
@@ -1,24 +1,40 @@
 package pl.gccalc;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
     static Aircraft aircraft = new Aircraft();
     public static void main(String[] args) {
         System.out.println("Hello in calculator please enter correct values:");
-        System.out.println("enter l lenght:");
 
-        Scanner scanner = new Scanner(System.in);
-        aircraft.setLLenght(scanner.nextDouble());
+
+//        Scanner scanner = new Scanner(System.in);
+        BufferedReader odczyt = new BufferedReader(new InputStreamReader(System.in));
+
+        while(aircraft.getLLenght() == null)
+        {
+            try{
+                System.out.println("enter l lenght:");
+//                aircraft.setLLenght(scanner.nextDouble());
+                aircraft.setLLenght(Double.parseDouble(odczyt.readLine()));
+            } catch (Exception e) {
+                System.out.println("wrong!!!");;
+            }
+        }
+
+
+
 
         System.out.println("enter a lenght");
-        aircraft.setALenght(scanner.nextDouble());
+//        aircraft.setALenght(scanner.nextDouble());
 
         System.out.println("enter front weight");
-        aircraft.setFrontWeight(scanner.nextDouble());
+//        aircraft.setFrontWeight(scanner.nextDouble());
 
         System.out.println("enter rear weight");
-        aircraft.setRearWeihgt(scanner.nextDouble());
+//        aircraft.setRearWeihgt(scanner.nextDouble());
 
         Double result =  (((aircraft.getLLenght() * aircraft.getRearWeihgt())/(aircraft.getFrontWeight()+ aircraft.getRearWeihgt()))- aircraft.getALenght());
 
