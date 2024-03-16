97c29be8945101e6887b3339f88d920499bcf756
Marcin <awings@o2.pl>
2024-03-16 12:13:45 +0100

    Aircraft class update

--- Gravity_Center_Calculator/src/main/java/pl/gccalc/Aircraft.java
+++ Gravity_Center_Calculator/src/main/java/pl/gccalc/Aircraft.java
@@ -8,24 +8,42 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Aircraft {
-private Double frontWeight;
-private Double rearWeight;
-private Double aLength;
-private Double lLength ;
-private String weighingMethod ;
-private Double gravityCentre ;
 
+
+    private Double frontWeight;
+    private Double rearWeight;
+
+    private Double totalWeight;
+    private Double leftWingWeight;
+    private Double rightWingWeight;
+    private Double tailplaneWeight;
+    private Double fuselageWeight;
+    private Double aLength;
+    private Double lLength;
+    private String weighingMethod;
+    private Double gravityCentre;
+
+    public void setTotalWeight(Double frontWeight, Double rearWeight) {
+        this.totalWeight = frontWeight + rearWeight;
+    }
+
+    public void setFuselageWeight(Double totalWeight, Double leftWingWeight, Double rightWingWeight, Double tailplaneWeight) {
+        this.fuselageWeight = totalWeight - leftWingWeight  - rightWingWeight - tailplaneWeight;
+    }
 
     @Override
     public String toString() {
-        return "Aircraft: " +
+        return "Aircraft{" +
                 "frontWeight=" + frontWeight +
                 ", rearWeight=" + rearWeight +
+                ", totalWeight=" + totalWeight +
+                ", leftWingWeight=" + leftWingWeight +
+                ", rightWingWeight=" + rightWingWeight +
+                ", tailplaneWeight=" + tailplaneWeight +
+                ", fuselageWeight=" + fuselageWeight +
                 ", aLength=" + aLength +
                 ", lLength=" + lLength +
-                 "\n" +
-                ", Center of gravity position: = " + gravityCentre +
-                "\n" +
-                "Please check range of G.C. in Maintenance manual of your glider." ;
+                ", gravityCentre=" + gravityCentre +
+                '}';
     }
 }
