f1b8278670d8ccace9d963f56c58fcde7978428e
Marcin <awings@o2.pl>
2024-03-23 16:41:10 +0100

    pdf table

--- Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
+++ Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
@@ -15,6 +15,7 @@ public class Main {
         createPdf(aircraft);
     }
 
+
 //        System.out.print(ConsoleColors.BLUE);
 //        System.out.println("Welcome in calculator please enter correct values:" +  "USE DOT '.' AS DECIMAL"  + "\n" + "Read Maintenance Manual of Your glider to see weighing procedure." );
 //        System.out.print(ConsoleColors.RESET);
