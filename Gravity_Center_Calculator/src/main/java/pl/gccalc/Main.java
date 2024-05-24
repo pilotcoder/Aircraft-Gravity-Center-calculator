6c0f4a00d4220f066f1c24586f61fc718b1d5b4f
pilotcoder <awings@o2.pl>
2024-05-24 13:55:26 +0200

    null pointer in pdf fixed

--- Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
+++ Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
@@ -12,7 +12,7 @@ public class Main {
     static Aircraft aircraft = new Aircraft();
 
     public static void main(String[] args) throws DocumentException, FileNotFoundException {
-        createPdf(aircraft);
+//        createPdf(aircraft);
 
         System.out.print(ConsoleColors.BLUE);
         System.out.println("Welcome in calculator please enter correct values:" +  "USE DOT '.' AS DECIMAL"  + "\n" + "Read Maintenance Manual of Your glider to see weighing procedure." );
