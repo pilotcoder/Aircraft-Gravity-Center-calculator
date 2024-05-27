5eb90e9dea1d6805d51a88823df9187fd4484b8f
pilotcoder <awings@o2.pl>
2024-05-27 14:44:00 +0200

    update

--- Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
+++ Gravity_Center_Calculator/src/main/java/pl/gccalc/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) throws DocumentException, FileNotFoundException {
 //        createPdf(aircraft);
-
+90
         System.out.print(ConsoleColors.BLUE);
         System.out.println("Welcome in calculator please enter correct values:" +  "USE DOT '.' AS DECIMAL"  + "\n" + "Read Maintenance Manual of Your glider to see weighing procedure." );
         System.out.print(ConsoleColors.RESET);
